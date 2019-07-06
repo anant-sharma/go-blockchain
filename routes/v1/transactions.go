3b4072d83006d4b8faa9c4dfe776518eacbedc2d
Anant Sharma <anantatnet@gmail.com>
2019-07-06 23:47:46 +0530

    Updated Transaction API

--- routes/v1/transactions.go
+++ routes/v1/transactions.go
@@ -1,16 +1,28 @@
 package v1router
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/anant-sharma/go-blockchain/controller/v1/blockchain"
 	"github.com/gin-gonic/gin"
 )
 
+type transactionInput struct {
+	Amount    float64
+	Sender    string
+	Recipient string
+}
+
 // InitTransactionRouter function
 func InitTransactionRouter(router *gin.RouterGroup) {
 	router.POST("", func(ctx *gin.Context) {
-		transaction := blockchain.NewTransaction(100, "a", "b")
+
+		form, _ := ctx.GetRawData()
+		var tx transactionInput
+		json.Unmarshal(form, &tx)
+
+		transaction := blockchain.NewTransaction(tx.Amount, tx.Sender, tx.Recipient)
 		ctx.JSON(http.StatusOK, transaction)
 	})
 }
